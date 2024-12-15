220c7982ba410763e906277d99d5120c016e5fc9
Hakeem <hazaak009@gmail.com>
2024-12-15 23:29:05 +0100

    navbar progress

--- app/Components/Nav/Footer.tsx
+++ app/Components/Nav/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-[#1F1F1D] text-[#E7E0D8]'>Footer</div>
   )
 }
 
-export default Footer
+export default Footer 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Container from "./Components/Container";
 
-const page = () => {
+const Home = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="bg-[#f5efe7] min-h-screen">
+      <Container>
+        <div>page</div>
+      </Container>
+    </div>
+  );
+};
 
-export default page
+export default Home;
