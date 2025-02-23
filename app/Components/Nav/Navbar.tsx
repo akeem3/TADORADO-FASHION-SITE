c7ebfe69f421ad65b800c6529b3079567d781158
Hakeem <hazaak009@gmail.com>
2025-02-23 22:22:29 +0100

    commit

--- app/Components/Nav/Navbar.tsx
+++ app/Components/Nav/Navbar.tsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
-
-
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <header className="sticky top-0 flex items-center justify-between p-4 backdrop-blur-sm z-20">
+      
+      <div className="">
+        <Image
+          src="https://firebasestorage.googleapis.com/v0/b/tadorado-tailors.firebasestorage.app/o/Tadorado%20Logo%20SVG.svg?alt=media&token=c8e277a7-321e-4e75-81f7-d7f8c28cb86a"
+          alt=""
+          width={40}
+          height={40}
+          className="object-contain scale-110 lg:scale-120"
+          priority
+        />
+      </div>
       <Link
         href="/"
         className="writing-vertical-lr transform leading-none text-[#5d4037] text-xl font-semibold space-y-1"
@@ -12,6 +22,8 @@ export default function Navbar() {
         <div>Tadorado</div>
         <div>Tailoring</div>
       </Link>
+      
+
       <nav className="flex-grow">
         <ul className="flex justify-center space-x-8">
           <li>
