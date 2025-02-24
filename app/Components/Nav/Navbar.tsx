3074bfe9db91be7151f412cf76e1f1d5a4fc7311
Hakeem <hazaak009@gmail.com>
2025-02-24 15:38:30 +0100

    commit

--- app/Components/Nav/Navbar.tsx
+++ app/Components/Nav/Navbar.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <header className="sticky top-0 flex items-center justify-between p-4 backdrop-blur-sm z-20">
-      
       <div className="">
         <Image
           src="https://firebasestorage.googleapis.com/v0/b/tadorado-tailors.firebasestorage.app/o/Tado%20logo%20Final.png?alt=media&token=2cac7d0f-db1e-4a38-88c0-58b79a4eb5c7"
@@ -22,7 +21,6 @@ export default function Navbar() {
         <div>Tadorado</div>
         <div>Tailoring</div>
       </Link>
-      
 
       <nav className="flex-grow">
         <ul className="flex justify-center space-x-8">
@@ -34,28 +32,23 @@ export default function Navbar() {
               Home
             </Link>
           </li>
-          <li>
-            <Link
-              href="/services"
-              className="text-[#5d4037] hover:text-[#8d6e63] transition-colors duration-200"
-            >
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/about"
-              className="text-[#5d4037] hover:text-[#8d6e63] transition-colors duration-200"
-            >
-              About
-            </Link>
-          </li>
+
+
           <li>
             <Link
               href="/collections"
               className="text-[#5d4037] hover:text-[#8d6e63] transition-colors duration-200"
             >
               Collections
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/about"
+              className="text-[#5d4037] hover:text-[#8d6e63] transition-colors duration-200"
+            >
+              Blog
             </Link>
           </li>
           <li>
--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,13 @@ const Home = () => {
   return (
     <Container>
       {/* Banner Section */}
-      <div
-        className={
-          "flex flex-col lg:flex-row items-center justify-between px-8 lg:px-16 pb-[10] lg:pb-[8]"
-        }
-      >
+      <div className="flex flex-col lg:flex-row items-center justify-between px-8 lg:px-16 pb-10 lg:pb-20 relative">
         {/* Text Section */}
-        <div className="flex-1 max-w-2xl">
-          <h1 className="text-4xl lg:text-7xl xl:text-8xl font-bold text-[#46332E] mb-8 leading-tight">
+        <div className="flex-1 max-w-2xl text-center lg:text-left">
+          <h1 className="text-4xl lg:text-7xl font-bold text-[#46332E] mb-6 leading-tight">
             BOLD STYLE <br /> EVERY DAY
           </h1>
-          <p className="text-[#46332E] text-base lg:text-lg leading-relaxed mb-8 max-w-xl">
+          <p className="text-[#46332E] text-base lg:text-lg leading-relaxed mb-6 max-w-xl">
             Lorem ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry&apos;s standard dummy
             text.
@@ -25,51 +21,78 @@ const Home = () => {
           <Button variant="default">Contact</Button>
         </div>
 
-        {/* Image */}
-        <div className="flex-1 flex justify-center lg:justify-end mt-12 lg:mt-0">
+        {/* Image Section */}
+        <div className="flex-1 flex justify-center lg:justify-end lg:mt-0 relative">
           <Image
             src="https://firebasestorage.googleapis.com/v0/b/tadorado-tailors.firebasestorage.app/o/fghdfhdhx.png?alt=media&token=8b763577-41c5-43d3-a6d1-f1123d938e76"
             alt="Traditional Fashion"
-            width={600}
-            height={800}
-            className="object-contain scale-110 lg:scale-120"
+            width={700}
+            height={900}
+            className="object-contain relative z-10"
             priority
           />
         </div>
       </div>
 
+
+
       {/* About Section */}
-      <div className="bg-[#1F1F1D] w-full ">
-        <div>
-          <Image
-            src="https://firebasestorage.googleapis.com/v0/b/tadorado-tailors.firebasestorage.app/o/The%20besties.jpg?alt=media&token=6e218dd5-f398-410e-ac7e-a4f05ff40b51"
-            alt="Traditional Fashion"
-            width={350}
-            height={650}
-            className="object-contain scale-110 lg:scale-120"
-            priority
-          />
-        </div>
+      <div className="bg-[#1F1F1D] w-full py-4 px-8 lg:px-16 relative">
+        <div className="grid lg:grid-cols-2 gap-20 items-center relative">
+          {/* Left Image */}
+          <div className="flex justify-center relative">
+            <Image
+              src="https://firebasestorage.googleapis.com/v0/b/tadorado-tailors.firebasestorage.app/o/The%20besties.jpg?alt=media&token=6e218dd5-f398-410e-a4f05ff40b51"
+              alt="fashion"
+              width={700}
+              height={900}
+              className="object-cover rounded-lg relative lg:-top-16"
+              priority
+            />
+          </div>
 
-        <div className="text-white py-10 flex-1 max-w-2xl ">
-          <h2 className="text-4xl font-bold mb-4">About Section</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit
-            arcu in pretium molestie. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus.
-          </p>
-          <Button variant="default">Contact</Button>
+          {/* Text */}
+          <div className="text-white max-w-xl relative">
+            <h2 className="text-4xl font-bold mb-6">ABOUT TADORADO</h2>
+            <p className="text-lg leading-relaxed mb-6">
+              Our designs are a blend of tradition and modernity, creating
+              pieces that stand the test of time. Experience craftsmanship like
+              never before.
+            </p>
+            <Button
+              variant="default"
+              className="mb-6 px-6 py-7 text-lg font-semibold"
+            >
+              Read More
+            </Button>
+            {/* Sewing Image */}
+            <div className="flex justify-start relative">
+              <Image
+                src="https://firebasestorage.googleapis.com/v0/b/tadorado-tailors.firebasestorage.app/o/sewing%20image.jpg?alt=media&token=0ca23b9a-dfe4-4b89-a32b-8ea93475a352"
+                alt="Sewing"
+                width={450}
+                height={350}
+                className="object-cover rounded-lg relative lg:top-10"
+                priority
+              />
+            </div>
+          </div>
         </div>
+      </div>
 
+
+
+      {/* Categories Section */}
+      <div>
         <div>
-        <Image
-            src="https://firebasestorage.googleapis.com/v0/b/tadorado-tailors.firebasestorage.app/o/sewing%20image.jpg?alt=media&token=0ca23b9a-dfe4-4b89-a32b-8ea93475a352"
-            alt="Traditional Fashion"
-            width={350}
-            height={650}
-            className="object-contain scale-110 lg:scale-120"
-            priority
-          />
+          {/* Text */}
+          <div className="max-w-xl">
+            <h2 className="text-4xl font-bold mb-6">VIEW OUR CATEGORIES</h2>
+            <p className="text-lg leading-relaxed mb-6">
+              Lorem ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been theLorem{" "}
+            </p>
+          </div>
         </div>
       </div>
     </Container>
