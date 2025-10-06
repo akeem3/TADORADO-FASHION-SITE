1ac25111f946da9c79a2793f8d57731c1fadaea5
Hakeem <hazaak009@gmail.com>
2025-10-06 22:19:58 +0100

    commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    // Bypass Next.js image optimizer so remote Firebase images load directly in prod
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
